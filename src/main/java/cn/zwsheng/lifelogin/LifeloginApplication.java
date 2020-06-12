2f0b634f6b7c6a12519801487955de2ddefba97f
zhengws <zhengws@ubox.cn>
2020-06-12 17:00:52 +0800

    Initial commit

---
+++ src/main/java/cn/zwsheng/lifelogin/LifeloginApplication.java
@@ -0,0 +1,13 @@
+package cn.zwsheng.lifelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zwsheng/lifelogin/LifeloginApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zwsheng.lifelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
